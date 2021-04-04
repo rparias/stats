bcacb0b80726478de2720c0f157fa53ca956c413
Ronald Arias <ronald.arias@thoughtworks.com>
2021-04-04 18:11:54 -0500

    Loads and Parses the information from the csv file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import { readFileSync } from 'fs';
+
+const matches = readFileSync('football.csv', {
+  encoding: 'utf-8',
+})
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+console.log(matches);
